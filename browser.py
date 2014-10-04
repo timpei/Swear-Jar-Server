580ec10ac39148a9bd99cd6e8b42abab8bf5c16e
Tim Pei <timtimpei@hotmail.com>
2014-10-04 03:09:13 -0700

    added api functions

--- browser.py
+++ browser.py
@@ -20,5 +20,29 @@ def teardown_request(exception):
     # closes the db instance coneection if needed
     return
 
+@app.route('/message/send', methods=['POST'])
+def logSentMessage():
+	message = request.form['message']
+	sendFrom = request.form['contactFrom']
+	sendTo = request.form['contactTo']
+	return jsonify(**{
+		"score": 0,
+		"jarSum": 0
+		})
+
+@app.route('/message/receieve', methods=['POST'])
+def logReceiveMessage():
+	message = request.form['message']
+	sendFrom = request.form['contactFrom']
+	sendTo = request.form['contactTo']
+	return jsonify(**{
+		"success": 0
+		})
+
+@app.route('/score', methods=['GET'])
+def getScore():
+	return jsonify(**{"score": 0})
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
