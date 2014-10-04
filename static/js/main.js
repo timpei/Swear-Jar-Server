323c0c26e265ef3114470b6d39f772dd792d5470
topher-stern <topher.stern@gmail.com>
2014-10-04 07:45:09 -0700

    Added facebook login implementation

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,54 @@
  *   */
 var app = angular.module('swearJar', [
 ]);
+
+SCOPE = null;
+
+app.controller('SwearJarController', function($scope){
+  fbInit($scope);
+  SCOPE = $scope;
+  $scope.phoneNumber = '0-';
+  $scope.loggedIn = false;
+  $scope.login = function() {
+    console.log('login');
+    FB.login(function(response){
+      statusChangeCallback(response, $scope);
+    });
+  };
+});
+
+var loadDashboard= function($scope){
+  FB.api('/me', {fields: 'first_name, last_name'}, function(response){
+    debugger;
+    $scope.userName = response.first_name + ' ' + response.last_name;
+  });
+};
+
+var statusChangeCallback = function(response, $scope){
+  debugger;
+  if(response.status == 'connected'){
+    $scope.loggedIn = true;
+    loadDashboard($scope);
+  }
+};
+var fbInit = function($scope){
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '661005694006952',
+      xfbml      : true,
+      version    : 'v2.1'
+    });
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response, $scope);
+    });
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    
+  }(document, 'script', 'facebook-jssdk'));
+};
