a94a0b9292fb9cea9d84b15e4ade22691fe0c928
Tim Pei <timtimpei@hotmail.com>
2014-10-04 16:11:38 -0700

    Fixing port issue.

--- browser.py
+++ browser.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 from flask import Flask, request, Response, session, g, redirect, url_for, abort, render_template, flash, jsonify
 
 
-DB_URL = "mongodb://admin:admin@ds043220.mongolab.com:43220/swearjar"
+DB_URL = "mongodb://calhacks:12345@ds043220.mongolab.com:43220/swearjar"
 DEBUG = True
 
 app = Flask(__name__)
