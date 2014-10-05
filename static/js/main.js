b3ab7eae4f9cadbdd3e94108de183bd2a01aceab
chrisngan24 <chris.ngan24@gmail.com>
2014-10-05 04:52:56 -0400

    fix fb bug

--- static/js/main.js
+++ static/js/main.js
@@ -27,16 +27,29 @@ var statusChangeCallback = function(response, $scope){
   }
 };
 var fbInit = function($scope){
+  /*
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '661005694006952',
       xfbml      : true,
       version    : 'v2.1'
     });
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      
+    }(document, 'script', 'facebook-jssdk'));
+
+  
+  };
+  */
+  $(document).ready(function(){
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response, $scope);
     });
-  };
-
+  });
 };
 
