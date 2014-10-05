f36347316e1244cd98404d5de443a7ae6c08d44b
chrisngan24 <chris.ngan24@gmail.com>
2014-10-05 06:55:31 -0400

    change from day stamp to time stamp

--- static/js/charting.js
+++ static/js/charting.js
@@ -72,7 +72,7 @@ charting.drawTimeseriesChart = function(list, targetId){
     chart.xAxis
       .axisLabel('Date')
       .tickFormat(function(d) {
-        return d3.time.format('%x')(new Date(d))
+        return d3.time.format('%I:%M %p')(new Date(d))
                                   });
     chart.yAxis
       .axisLabel('Swear Volume');
