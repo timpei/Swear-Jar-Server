56c138a0178423224265c2c876d3b5993526314c
Tim Pei <timtimpei@hotmail.com>
2014-10-05 04:26:53 -0700

    front page templating

---
+++ static/js/agency.js
@@ -0,0 +1,10 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
--- static/js/main.js
+++ static/js/main.js
@@ -38,13 +38,5 @@ var fbInit = function($scope){
     });
   };
 
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    
-  }(document, 'script', 'facebook-jssdk'));
 };
 
